6d2dd550815becc44ed9475876970430350979d7
thedocastrex <lorenzobusicchia87@gmail.com>
2023-11-16 12:46:26 +0100

    Inserting class with property and decorators

--- classWithProperty.py
+++ classWithProperty.py
@@ -3,21 +3,20 @@ class MyClass():
     def __init__(self, my_attr):
         self.__priv_attr=my_attr
 
-    def get_attr(self):
+    @property #decorator for getter method
+    def attr(self):
         return self.__priv_attr
-
-    def set_attr(self,attr):
-        self.__priv_attr=attr
     
-    attr = property(get_attr,set_attr)
+    @attr.setter #decorator for setter method
+    def attr(self,attr):
+        self.__priv_attr=attr
+     
 
 obj = MyClass('python')
-print(obj.attr)
-obj.attr='prova'
-print(obj.attr)
+print(obj.attr)  
+obj.attr='hello'
+print(obj.attr)  
 
-obj.set_attr('the try')
-print(obj.get_attr())
 #print(obj.__priv_attr)
 
 #attributes that are preceded by double underscore are not visible at the outside of class
