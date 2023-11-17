eda3e0cb7e7ce4dafda404d1b3ba45a9c4be07d1
thedocastrex <lorenzobusicchia87@gmail.com>
2023-11-17 14:03:50 +0100

    Class with Exceptions using try, except, else, finally, raise, assert

--- withExceptions.py
+++ withExceptions.py
@@ -3,10 +3,36 @@ class TheClass:
     def divide(a,b):
         try:
             return a//b
-        except ZeroDivisionError:
-            return "You can't divide by zero"
+        except ZeroDivisionError as e:
+            print(e.args)
+        else:
+            print(" 'Else' can be used just after all 'except' claues and befory 'finally' ")
+            print(" 'Else' is invoked if no exception happens ")
+        finally:
+            print("I am executed always")
+     
+    def divid(a,b): 
+        return a//b
     
-z=TheClass.divide(1,0)
-print(z)
+    
+#z=TheClass.divide(1,0)
+#print(z)
 y=TheClass.divide(30,10)
 print(y)
+x=TheClass.divide(30,1)
+print(x)
+
+#RAISE
+
+#for i in range(50):
+    #print(i)
+    #raise IndexError("Loop error")
+ 
+#try:
+#    c=TheClass.divid(4,0)
+#except ZeroDivisionError:
+#    print("Error div 0")
+#    raise
+
+x = 10
+assert x == 5, "Not equal to 5"
